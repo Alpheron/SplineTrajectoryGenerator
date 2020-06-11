c940035b781410dcfcacc4691f349a2b1aca83a3
Aditya Mangalampalli <aditya.mangalampalli@gmail.com>
2020-06-11 10:57:02 -0700

    Make robot render on the start point
    Signed-off-by: Aditya Mangalampalli <aditya.mangalampalli@gmail.com>

--- visualizer/main.py
+++ visualizer/main.py
@@ -7,14 +7,15 @@ from neural_network.pose import Pose2D
 from visualizer.field import Field
 from visualizer.point import Point
 from visualizer.robot import Robot
-from visualizer.trail import Trail
 from visualizer.utils import SCREEN_DIM
 
 
 class Main:
 
-    def __init__(self):
+    def __init__(self, startPose, endPose):
         self.started = False
+        self.startPose = startPose
+        self.endPose = endPose
         self.screen = pygame.display.set_mode([SCREEN_DIM, SCREEN_DIM])
         self.robot = Robot(screen=self.screen)
         self.field = Field(screen=self.screen)
@@ -24,11 +25,10 @@ class Main:
 
     def updateRobot(self, pose):
         self.screen.blit(self.robot.rotate(-pose.getTheta()), self.robot.move(pose.getX(), -pose.getY()))
-        trail = Trail(pose.getX(), -pose.getY(), self.screen)
 
-    def createPoints(self, startPose, endPose):
-        startPoint = Point(startPose.getX(), -startPose.getY(), self.screen)
-        endPoint = Point(endPose.getX(), -endPose.getY(), self.screen)
+    def createPoints(self):
+        startPoint = Point(self.startPose.getX(), -self.startPose.getY(), self.screen)
+        endPoint = Point(self.endPose.getX(), -self.endPose.getY(), self.screen)
         return startPoint, endPoint
 
     def quitLoopConditional(self):
@@ -48,11 +48,11 @@ class Main:
             self.quitLoopConditional()
             self.screen.fill([255, 255, 255])
             self.screen.blit(self.field.image, self.field.rect)
-            self.createPoints(Pose2D(-40, -40), Pose2D(72, 0))
-            self.updateRobot(self.testPose.random())
+            self.createPoints()
+            self.updateRobot(self.startPose)
             time.sleep(2)
             pygame.display.update()
 
 
 if __name__ == '__main__':
-    window = Main()
+    window = Main(Pose2D(-36, 0, 40), Pose2D(56, 37, 42))
--- visualizer/robot.py
+++ visualizer/robot.py
@@ -10,19 +10,19 @@ class Robot(pygame.sprite.Sprite):
     def __init__(self, screen):
         pygame.sprite.Sprite.__init__(self)
         self.image = load_image(18, '/home/tinku/SplineTrajectoryGenerator/visualizer/assets/Robot.png')
+        self.transFormImage = None
         self.rect = self.image.get_rect()
         self.rect.center = [400, 400]
         self.screen = screen
 
     def move(self, x, y):
-        newRobotRect = tuple([scaled_value(x, isRelative=True), scaled_value(y, isRelative=True)])
-        print(scaled_value(x, isRelative=True))
-        print(scaled_value(y, isRelative=True))
+        newRobotRect = self.transFormImage.get_rect()
+        newRobotRect.center = tuple([scaled_value(x, isRelative=True), scaled_value(y, isRelative=True)])
         return newRobotRect
 
     def rotate(self, angle):
         rot_image = pygame.transform.rotate(self.image, degrees(angle))
         rot_rect = rot_image.get_rect()
         rot_image = rot_image.subsurface(rot_rect).copy()
-        rot_rect.clamp(self.screen.get_rect())
+        self.transFormImage = rot_image
         return rot_image
